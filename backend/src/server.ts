b83bfc23be757e30e585d269e3fa77e85f8b9aea
Clément Trotignon <ctrotignon@kameleoon.com>
2023-10-23 11:56:15 +0200

    Create server and set tsconfig

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,26 +1,19 @@
-import express from "express";
-import cors from 'cors';
-import dotenv from 'dotenv';
-import mysql from 'mysql';
+import express, { Application, Request, Response } from 'express'
 
-const app = express();
-app.use(cors());
-dotenv.config();
-const PORT : string | number = process.env.PORT || 5000;
+const app: Application = express()
+const PORT = 3000
 
-
-const pool = mysql.createPool({
-        connectionLimit: 10,
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'quizzin_DB',
+// Body parsing Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get('/', async(req: Request, res: Response): Promise<Response> => {
+    return res.status(200).send({ message: `Welcome to the quizzin API!` })
 })
 
-
-app.get('/', (req, res) => {
-    res.sendStatus(200);
-})
-app.listen(PORT, () => {
-    console.log(`Server running on: ${PORT}`);
-})
+try {
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`)
+    })
+} catch (error : unknown) {
+    if (error instanceof TypeError) console.log(`Error occurred: ${error.message}`)
+}
