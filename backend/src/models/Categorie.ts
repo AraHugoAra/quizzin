a58cca4d69f445f91f304fc958852a6b30764256
Clément Trotignon <ctrotignon@kameleoon.com>
2023-10-25 11:40:44 +0200

    updating Categories Model

--- backend/src/models/Categorie.ts
+++ backend/src/models/Categorie.ts
@@ -1,13 +1,9 @@
-import { Sequelize, DataTypes } from "sequelize";
-const sequelize = new Sequelize("sqlite::memory:");
+import { DataTypes } from "sequelize";
+import { sequelizeConfig } from "../dbConfig";
 
-const Categorie = sequelize.define("Categorie", {
-  // Model attributes are defined here
+const Categorie = sequelizeConfig.define("Categorie", {
   categorieName: {
     type: DataTypes.STRING,
     allowNull: false
   }
 });
-
-// `sequelize.define` also returns the model
-console.log(Categorie === sequelize.models.Categorie); // true
