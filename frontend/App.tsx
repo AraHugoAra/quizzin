39cac4109d8a70691111c48b251279cb6aa6ca70
Yiyi <loveloveyy@hotmail.com>
2023-10-27 12:01:47 +0200

    creation screens, uninstall expo router

---
+++ frontend/App.tsx
@@ -0,0 +1,42 @@
+import { StyleSheet, Text, View } from "react-native";
+import SignupScreen from "./containers/SignupScreen";
+import ChallengeRankingScreen from "./containers/ChallengeRankingScreen";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.titleBlock}>
+        <Text style={styles.titleQuizz}>Quizz</Text>
+        <Text style={styles.titleIn}>in</Text>
+      </View>
+      <SignupScreen />
+      {/* <ChallengeRankingScreen /> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFEDE1",
+    alignItems: "center"
+  },
+
+  titleBlock: {
+    flexDirection: "row",
+    marginTop: 65,
+    justifyContent: "center"
+  },
+
+  titleQuizz: {
+    fontSize: 60,
+    color: "#ED6931",
+    fontWeight: "bold"
+  },
+
+  titleIn: {
+    fontSize: 60,
+    color: "#fa003f",
+    fontWeight: "bold"
+  }
+});
---
+++ frontend/containers/SignupScreen.tsx
@@ -0,0 +1,45 @@
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import Button from "../components/button";
+
+export default function SignupScreen() {
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your username..."
+      ></TextInput>
+      <Text style={styles.infoText}>
+        No blank spaces nor special characters
+      </Text>
+
+      <Button
+        backgroundColor="#06C699"
+        text="OK"
+        onPress={() => {
+          console.log("ChallengeRankingScreen");
+        }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center"
+  },
+
+  input: {
+    marginTop: 300,
+    marginBottom: 6,
+    width: "85%",
+    height: "10%",
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 10,
+    fontSize: 20
+  },
+
+  infoText: {
+    fontStyle: "italic"
+  }
+});
