5062cc61988be356cd8023b77b1e948d3399dcef
Clément Trotignon <ctrotignon@kameleoon.com>
2023-11-21 12:07:04 +0100

    updating controller User

--- backend/src/controllers/User.ts
+++ backend/src/controllers/User.ts
@@ -3,8 +3,10 @@ import { Request, Response, NextFunction } from "express";
 
 const register = async (req: Request, res: Response) => {
   try {
-    const user = await User.create({ username: req.body.username });
+    const user = await User.create({ userName: req.body.userName });
     console.log(user);
+    res.status(201).json({message : 'top ça marche'})
+    return
   } catch (error) {
     throw new Error(`Unable to create user, ${error}`);
   }
@@ -12,8 +14,9 @@ const register = async (req: Request, res: Response) => {
 
 const login = async (req: Request, res: Response) => {
   try {
-    const user = await User.findOne({ where: { username: req.body.username } });
+    const user = await User.findOne({ where: { userName: req.body.userName } });
     console.log(user);
+    res.status(200).json({message : 'bien loggé '})
   } catch (error) {
     throw new Error(`Unable to login, ${error}`);
   }
