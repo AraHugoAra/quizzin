e3b019edf4aa93f70b047718d0886b86166b19e5
fvilmot <fvilmot@gmail.com>
2023-11-21 16:26:11 +0100

    QI-20 : add routes and controller for weekly and monthly quizz

--- backend/src/controllers/Quiz.ts
+++ backend/src/controllers/Quiz.ts
@@ -2,6 +2,33 @@ import { Quiz } from "../models/index";
 import { Request, Response, NextFunction } from "express";
 import axios from "axios";
 
+const URL_API = "https://opentdb.com/api.php?amount=10&difficulty=hard&type=multiple";
+
+function getQuiz(quizType: string, dateToSearch: Date, res: Response): void {
+  let quizObject = {
+    questions: [],
+    date: dateToSearch,
+    quizType: quizType,
+  };
+
+  axios
+    .get(URL_API)
+    .then((resAPI) => {
+      quizObject.questions = resAPI.data.results;
+
+      Quiz.create({
+        questions: quizObject.questions,
+        date: quizObject.date,
+        quizType: quizObject.quizType,
+      })
+        .then((result) => res.status(201).json(result))
+        .catch((error: any) => res.status(400).json({ error }));
+    })
+    .catch((errAPI) => {
+      res.status(500).json({ errAPI });
+    });
+}
+
 export const getDailyQuiz = (
   req: Request,
   res: Response,
@@ -17,32 +44,56 @@ export const getDailyQuiz = (
       if (result !== null) {
         res.status(201).json(result);
       } else {
-        //  return console.log("pas de reponse");
+        getQuiz("daily", dateToSearch, res);
+      }
+    })
+    .catch((error: any) => res.status(500).json({ error }));
+};
 
-        const url = "https://opentdb.com/api.php?amount=10&type=multiple";
+export const getWeeklyQuiz = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  const today = new Date();
+  const dayOfWeek = today.getDay();
+  const diff = today.getDate() - dayOfWeek + (dayOfWeek === 0 ? -6 : 1);
+  const firstMondayOfTheWeek = new Date(today.setDate(diff));
+  const dateToSearch = new Date(
+    firstMondayOfTheWeek.toISOString().split("T")[0]
+  );
 
-        let quizObject = {
-          questions: [],
-          date: Date.now(),
-          quizType: "daily",
-        };
+  Quiz.findOne({
+    where: { date: today, quizType: "weekly" },
+  })
+    .then((result) => {
+      if (result !== null) {
+        res.status(201).json(result);
+      } else {
+        getQuiz("weekly", dateToSearch, res);
+      }
+    })
+    .catch((error: any) => res.status(500).json({ error }));
+};
 
-        axios
-          .get(url)
-          .then((resAPI) => {
-            quizObject.questions = resAPI.data.results;
+export const getMonthlyQuiz = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  const today = new Date();
+  const dateToSearch = new Date(today.getFullYear(), today.getMonth(), 1);
+  const offset = today.getTimezoneOffset() * 60 * 1000;
+  const localDateToSearch = new Date(dateToSearch.getTime() - offset);
 
-            Quiz.create({
-              questions: quizObject.questions,
-              date: quizObject.date,
-              quizType: quizObject.quizType,
-            })
-              .then((result) => res.status(201).json(result))
-              .catch((error: any) => res.status(400).json({ error }));
-          })
-          .catch((errAPI) => {
-            console.log(errAPI);
-          });
+  Quiz.findOne({
+    where: { date: localDateToSearch, quizType: "monthly" },
+  })
+    .then((result) => {
+      if (result !== null) {
+        res.status(201).json(result);
+      } else {
+        getQuiz("monthly", localDateToSearch, res);
       }
     })
     .catch((error: any) => res.status(500).json({ error }));
--- backend/src/routes/Quiz.ts
+++ backend/src/routes/Quiz.ts
@@ -2,6 +2,8 @@ import express from "express";
 
 export const quizRoutes = express.Router();
 
-import { getDailyQuiz } from "../controllers/Quiz";
+import { getDailyQuiz, getWeeklyQuiz, getMonthlyQuiz } from "../controllers/Quiz";
 
 quizRoutes.post("/dailyquiz", getDailyQuiz);
+quizRoutes.post("/weeklyquiz", getWeeklyQuiz);
+quizRoutes.post("/monthlyquiz", getMonthlyQuiz);
